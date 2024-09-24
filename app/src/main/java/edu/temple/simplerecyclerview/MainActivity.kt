506c497e566820e95a1814af5f8b28112dfc3d6c
rahrah-7 <orah.smith@temple.edu>
2024-09-24 15:16:44 -0400

    finished activity

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,16 @@ class MainActivity : AppCompatActivity() {
 
         //TODO Step 2: Provide a LayoutManager
 
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+
 
         //TODO Step 4: Provide a RecyclerView.Adapter
+
+        val numField = IntArray(20 ) {(it+1) *5}
+
+        recyclerView.adapter = NumberDisplayAdapter(numField)
+
     }
 }
